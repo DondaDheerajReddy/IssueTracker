44ec635232cd75246b724767db0684dbb5628f6e
Donda Dheeraj <dheeraj2629@gmail.com>
2025-07-22 20:59:02 +0530

    style navbar active links

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,20 +1,38 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import { FaBug } from "react-icons/fa6";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 const Navbar = () => {
   const links = [
-    {label: "Dashboard", href: "/"},
-    {label: "Issues", href: "/issues"}
-  ]
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  const currentPath = usePathname();
   return (
-    <nav className="flex space-x-6 px-5 border-b h-14 mb-5 items-center" >
-      <Link href="/"><FaBug /></Link>
-      <ul className="flex space-x-6" >
+    <nav className="flex space-x-6 px-5 border-b h-14 mb-5 items-center">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className="flex space-x-6">
         {links.map((link, id) => {
-          return <li key={id} >
-            <Link className="text-zinc-600 hover:text-zinc-900 transition-colors "  href={link.href} >{link.label}</Link>
-          </li>
+          return (
+            <li key={id}>
+              <Link
+                className={classnames({
+                  "text-zinc-900": link.href === currentPath,
+                  "text-zinc-500": link.href !== currentPath,
+                  "hover:text-zinc-800 transition-colors": true,
+                })}
+                href={link.href}
+              >
+                {link.label}
+              </Link>
+            </li>
+          );
         })}
       </ul>
     </nav>
---
+++ app/issues/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PageFile = () => {
+  return (
+    <div>PageFile</div>
+  )
+}
+
+export default PageFile
